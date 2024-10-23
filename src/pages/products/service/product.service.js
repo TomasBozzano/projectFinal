b6cf31383fc70b07abb0516dd8fc499e9c1e42a6
TomasBozzano <tomycas10@gmail.com>
2024-10-23 18:32:26 -0300

    Product service

--- src/pages/products/service/product.service.js
+++ src/pages/products/service/product.service.js
@@ -5,3 +5,7 @@ const API_URL = import.meta.env.VITE_API_URL;
 export const getProducts = async() =>{
     return await axios.get(`${API_URL}/products`)
 }
+
+export const productById= async(id)=>{
+    return await axios.get(`${API_URL}/products/${id}`)
+}
