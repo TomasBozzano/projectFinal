12e9153f43f6c15e60e8e4f7cede5123efd4b0b9
TomasBozzano <tomycas10@gmail.com>
2024-10-23 18:35:17 -0300

    Take out console.log

--- src/pages/products/components/product-list.jsx
+++ src/pages/products/components/product-list.jsx
@@ -9,7 +9,6 @@ export function ProductList() {
             try {
                 const response = await getProducts()
                 const products = response.data.products
-                console.log(products)
                 SetList(products)
             } catch (error) {
                 message.error(error);
@@ -21,7 +20,7 @@ export function ProductList() {
     }, [])
   return (
     <section className='h-full space-y-6 flex flex-col gap-4 p-4'>
-        <div className="grid grid-cols-5  max-md:grid-cols-2  gap-4 max-h-full h-full overflow-auto  ">
+        <div className="grid grid-cols-3 max-md:grid-cols-1  gap-4 max-h-full h-full overflow-auto  ">
             {list.map((product) => (
                 <ProductCard product={product} key={product.id} />
             ))}
